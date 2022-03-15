bb7ca187c40937c676ca1ad3e31ea74bbdf9ff87
Rybik52 <endyse70@gmail.com>
2022-03-15 17:55:54 +0300

    test
    pull

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
+
+const favicon = require('serve-favicon');
+
 const express = require('express');
 const hbs = require("express-handlebars"); // Подключаем express-handlebars
-const favicon = require('serve-favicon');
 const { engine } = require('express-handlebars');
-const server = require("./server/routes/server"); // Подключаем экспортированную в Шаге 4 переменную
-const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
+
+const server = require("./server/routes/server");
+const mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 8080; // Константа для порта
 const PASS = process.env.PASS;
 const app = express();
